f63ad79214aeb5e373a2e844359636502ec1fbc9
Md. Touhid Alam <14317784+touhidalam69@users.noreply.github.com>
2018-05-18 03:11:05 +0600

    Update Config and app.js
    Added new function for sending Notification

--- src/app.js
+++ src/app.js
@@ -5,10 +5,38 @@ let steem = require('steem');
 
 config = JSON.parse(fs.readFileSync("config.json"));
 
-GetCurrentTransaction();
+let transporter = nodemailer.createTransport({
+    service: config.Service,
+    auth: {
+      user: config.Sender_Email,
+      pass: config.Password
+    }
+  });
+
+
+//GetCurrentTransaction();
+
 function GetCurrentTransaction() {
     steem.api.getAccountHistory(config.Steem_account, -1, 100, (err, result) => {
         let received = result.filter(tx => tx[1].op[0] === 'transfer' && tx[1].op[1].to === config.Steem_account);
         let sended = result.filter(tx => tx[1].op[0] === 'transfer' && tx[1].op[1].from === config.Steem_account);
     });
 }
+
+function SendNotification(mail, to, title, body){
+    let mailOptions = {
+        from: mail,
+        to: to,
+        subject: title,
+        text: body
+    };
+    
+    transporter.sendMail(mailOptions, function(err, res){
+        if(err) {
+            console.log('Mail Sending failed for '+err.message);
+        }else{
+            console.log('Notification Email Sent Successfully!');
+        }
+    });
+}
+
