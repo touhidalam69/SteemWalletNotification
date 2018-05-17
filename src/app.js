2679dd4e184beadc9f6f37a8dbbe007372ed09f4
Md. Touhid Alam <14317784+touhidalam69@users.noreply.github.com>
2018-05-18 02:27:59 +0600

    Update Config file and app.js
    Balance transfer history retrival function added on app.js as config file steem account

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
-// Added References of Nodemailer and Steemjs
+// Added References of Nodemailer and Steemjs and fs
+const fs = require("fs");
 let nodemailer = require('nodemailer');
 let steem = require('steem');
+
+config = JSON.parse(fs.readFileSync("config.json"));
+
+GetCurrentTransaction();
+function GetCurrentTransaction() {
+    steem.api.getAccountHistory(config.Steem_account, -1, 100, (err, result) => {
+        let received = result.filter(tx => tx[1].op[0] === 'transfer' && tx[1].op[1].to === config.Steem_account);
+        let sended = result.filter(tx => tx[1].op[0] === 'transfer' && tx[1].op[1].from === config.Steem_account);
+    });
+}
