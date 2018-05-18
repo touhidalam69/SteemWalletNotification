45b2d84d18461ce03dca9ba260e65856d6a375d5
Md. Touhid Alam <14317784+touhidalam69@users.noreply.github.com>
2018-05-19 02:54:09 +0600

    Added New Function on app.js
    Added New Function on app.js and Update config.json

--- src/app.js
+++ src/app.js
@@ -5,38 +5,67 @@ let steem = require('steem');
 
 config = JSON.parse(fs.readFileSync("config.json"));
 
+var tempTransId=[];
+
+
 let transporter = nodemailer.createTransport({
     service: config.Service,
     auth: {
-      user: config.Sender_Email,
-      pass: config.Password
+        user: config.Sender_Email,
+        pass: config.Password
     }
-  });
+});
 
 
-//GetCurrentTransaction();
+GetCurrentTransaction();
 
 function GetCurrentTransaction() {
-    steem.api.getAccountHistory(config.Steem_account, -1, 100, (err, result) => {
-        let received = result.filter(tx => tx[1].op[0] === 'transfer' && tx[1].op[1].to === config.Steem_account);
-        let sended = result.filter(tx => tx[1].op[0] === 'transfer' && tx[1].op[1].from === config.Steem_account);
+    steem.api.getAccountHistory(config.Steem_account, -1, 500, (err, result) => {
+        if (err) {
+            console.log('Failed to retrive transection Data for ' + err.message);
+        }
+        else {
+            let received = result.filter(tx => tx[1].op[0] === 'transfer' && tx[1].op[1].to === config.Steem_account);
+            let sended = result.filter(tx => tx[1].op[0] === 'transfer' && tx[1].op[1].from === config.Steem_account);
+
+            mailbody = mailbodymaker(received, "Received");
+            mailbody += mailbodymaker(sended, "Sent");
+            SendNotification(config.Sender_Email, config.Notification_Email, "Wallet Notification", mailbody);
+        }
     });
 }
 
-function SendNotification(mail, to, title, body){
+function mailbodymaker(transferlist, transferType) {
+    tempTransId=[];
+    var mailbody = "\n============================\n" + transferType + " \n============================\n";
+    transferlist.forEach(function (atransfer) {
+        if (transferType === "Received") {
+            mailbody += atransfer[1].op[1].amount + " " + transferType + " from " + atransfer[1].op[1].from + " at " + new Date(atransfer[1].timestamp).toString() + " \n";
+        }
+        else {
+            mailbody += atransfer[1].op[1].amount + " " + transferType + " to " + atransfer[1].op[1].to + " at " + new Date(atransfer[1].timestamp).toString() + " \n";
+        }
+
+        tempTransId.push(atransfer[1].trx_id)
+
+    });
+
+    return mailbody;
+}
+
+function SendNotification(mail, to, title, body) {
     let mailOptions = {
         from: mail,
         to: to,
         subject: title,
         text: body
     };
-    
-    transporter.sendMail(mailOptions, function(err, res){
-        if(err) {
-            console.log('Mail Sending failed for '+err.message);
-        }else{
+
+    transporter.sendMail(mailOptions, function (err, res) {
+        if (err) {
+            console.log('Mail Sending failed for ' + err.message);
+        } else {
             console.log('Notification Email Sent Successfully!');
         }
     });
 }
-
