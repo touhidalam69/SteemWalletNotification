78c210dc2d265fe1dba52e47ebdd88530668a19e
Md. Touhid Alam <14317784+touhidalam69@users.noreply.github.com>
2018-05-19 21:00:15 +0600

    Update app.js
    Added New functions SaveMailLog, GetExistiongEmail. Updated Other function

--- src/app.js
+++ src/app.js
@@ -5,8 +5,7 @@ let steem = require('steem');
 
 config = JSON.parse(fs.readFileSync("config.json"));
 
-var tempTransId=[];
-
+var tempTransId = [];
 
 let transporter = nodemailer.createTransport({
     service: config.Service,
@@ -15,7 +14,6 @@ let transporter = nodemailer.createTransport({
         pass: config.Password
     }
 });
-
 
 GetCurrentTransaction();
 
@@ -36,36 +34,70 @@ function GetCurrentTransaction() {
 }
 
 function mailbodymaker(transferlist, transferType) {
-    tempTransId=[];
-    var mailbody = "\n============================\n" + transferType + " \n============================\n";
+    var preEmailList = GetExistiongEmail();
+    var mailbody = "";
     transferlist.forEach(function (atransfer) {
-        if (transferType === "Received") {
-            mailbody += atransfer[1].op[1].amount + " " + transferType + " from " + atransfer[1].op[1].from + " at " + new Date(atransfer[1].timestamp).toString() + " \n";
-        }
-        else {
-            mailbody += atransfer[1].op[1].amount + " " + transferType + " to " + atransfer[1].op[1].to + " at " + new Date(atransfer[1].timestamp).toString() + " \n";
-        }
+        if (preEmailList.filter(tx => tx === atransfer[1].trx_id).length === 0) {
+            if (mailbody === "") {
+                mailbody = "\n============================\n" + transferType + " \n============================\n";
+            }
+            if (transferType === "Received") {
+                mailbody += atransfer[1].op[1].amount + " " + transferType + " from " + atransfer[1].op[1].from + " at " + new Date(atransfer[1].timestamp).toString() + " \n";
+            }
+            else {
+                mailbody += atransfer[1].op[1].amount + " " + transferType + " to " + atransfer[1].op[1].to + " at " + new Date(atransfer[1].timestamp).toString() + " \n";
+            }
 
-        tempTransId.push(atransfer[1].trx_id)
-
+            tempTransId.push(atransfer[1].trx_id)
+        }
     });
 
     return mailbody;
 }
 
 function SendNotification(mail, to, title, body) {
-    let mailOptions = {
-        from: mail,
-        to: to,
-        subject: title,
-        text: body
-    };
+    if (tempTransId.length > 0) {
+        let mailOptions = {
+            from: mail,
+            to: to,
+            subject: title,
+            text: body
+        };
 
-    transporter.sendMail(mailOptions, function (err, res) {
+        transporter.sendMail(mailOptions, function (err, res) {
+            if (err) {
+                console.log('Mail Sending failed for ' + err.message);
+            } else {
+                console.log('Notification Email Sent Successfully!');
+                SaveMailLog();
+            }
+        });
+    }
+    else{
+        console.log('No Transaction Found !!!');
+    }
+}
+
+function GetExistiongEmail() {
+    var preEmailList = [];
+    if (fs.existsSync('MailLog.json')) {
+        const preEmail = JSON.parse(fs.readFileSync("MailLog.json"));
+        if (preEmail.length) {
+            preEmailList = preEmail;
+        }
+    }
+
+    return preEmailList;
+}
+
+function SaveMailLog() {
+    var NewMailLog = GetExistiongEmail().concat(tempTransId);
+    fs.writeFile('MailLog.json', JSON.stringify(NewMailLog), function (err) {
         if (err) {
-            console.log('Mail Sending failed for ' + err.message);
-        } else {
-            console.log('Notification Email Sent Successfully!');
+            console.log(err);
+        }
+        else {
+            tempTransId = [];
         }
     });
 }
